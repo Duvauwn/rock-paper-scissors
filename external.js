a98d701b36f55d752f1ea3c759d46cc4a530efd7
Duvauwn <duvauwnprendergast@gmail.com>
2021-10-06 12:33:52 +0100

    external.js

--- external.js
+++ external.js
@@ -14,24 +14,69 @@ function computerPlay() {
         choice="Scissors"
         return choice;
     }
-    console.log(choice);
 }
 
 // The parameters that function playRound() takes
-const playerSelection="rock";
+const playerSelection=upper("paper")
 const computerSelection=computerPlay();
+//This function takes a string and returns the first letter capitalized
+function upper(player) {
+    let lower=player.toLowerCase();
+    let first=lower.charAt(0);
+    let upper=first.toUpperCase();
+    let final=lower.replace(first, upper);
+    return final;
+}
+//Logging the selections from the player and computer
+console.log("Player: "+playerSelection);
+console.log("Computer: "+computerSelection);
+
 
 //This function takes inputs and plays a single round
 function playRound(playerSelection,computerSelection) {
     let string;
-    if (playerSelection>computerSelection) {
-        string="You Win! This beats that";
+
+    //Section if player chooses rock
+    if (playerSelection==="Rock" && computerSelection==="Paper") {
+        string="You Lose! Paper beats Rock";
         return string;
-    } else if (playerSelection<computerSelection) {
-        string="You Lose! This beats that";
+    }
+    else if (playerSelection==="Rock" && computerSelection==="Scissors") {
+        string="You Win! Rock beats Scissors";
         return string;
-    } else {
+    }
+    else if (playerSelection==="Rock" && computerSelection==="Rock") {
         string="Tie! Play again";
         return string;
     }
+
+    //Section if player chooses paper
+    else if (playerSelection==="Paper" && computerSelection==="Paper") {
+        string="Tie! Play again";
+        return string;
+    }
+    else if (playerSelection==="Paper" && computerSelection==="Scissors") {
+        string="You Lose! Scissors beats Paper";
+        return string;
+    }
+    else if (playerSelection==="Paper" && computerSelection==="Rock") {
+        string="You Win! Paper beats Rock";
+        return string;
+    }
+
+    //Section if player chooses scissors
+    else if (playerSelection==="Scissors" && computerSelection==="Paper") {
+        string="You Win! Scissors beats Paper";
+        return string;
+    }
+    else if (playerSelection==="Scissors" && computerSelection==="Scissors") {
+        string="Tie! Play again";
+        return string;
+    }
+    else {
+        string="You Lose! Rock beats Scissors";
+        return string;
+    }
 }
+playRound(playerSelection, computerSelection);
+
