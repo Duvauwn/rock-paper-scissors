d3631482b958300e914f02ba2568e3c9c523a62f
Duvauwn Prendergast <duvauwnprendergast@gmail.com>
2021-10-11 12:09:37 +0100

    external.js index.html style.css

--- external.js
+++ external.js
@@ -1,95 +1,106 @@
 //This function returns a random choice for the computer
 function computerPlay() {
-    let rand = Math.floor(Math.random()*3)+1;
+    let rand = Math.floor(Math.random() * 3) + 1;
     let choice;
-    if (rand===1) {
-        choice="Rock"
+    if (rand === 1) {
+        choice = "Rock"
         return choice;
     }
-     else if(rand===2) {
-        choice="Paper"
+    else if (rand === 2) {
+        choice = "Paper"
         return choice;
     }
-     else {
-        choice="Scissors"
+    else {
+        choice = "Scissors"
         return choice;
     }
 }
 
-// The parameters that function playRound() takes
-let playerSelection=upper(prompt("Pick a move: "));
-let computerSelection=computerPlay();
+
 
 //This function takes a string and returns the first letter capitalized
 function upper(player) {
-    let lower=player.toLowerCase();
-    let first=lower.charAt(0);
-    let upper=first.toUpperCase();
-    let final=lower.replace(first, upper);
+    let lower = player.toLowerCase();
+    let first = lower.charAt(0);
+    let upper = first.toUpperCase();
+    let final = lower.replace(first, upper);
     return final;
 }
 
 
 //This function takes inputs and plays a single round
-function playRound(playerSelection,computerSelection) {
+function playRound(playerSelection, computerSelection) {
     let string;
 
     //Section if player chooses rock
-    if (playerSelection==="Rock" && computerSelection==="Paper") {
-        string="You Lose! Paper beats Rock";
+    if (playerSelection === "Rock" && computerSelection === "Paper") {
+        string = "You Lose! Paper beats Rock";
         return string;
     }
-    else if (playerSelection==="Rock" && computerSelection==="Scissors") {
-        string="You Win! Rock beats Scissors";
+    else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        string = "You Win! Rock beats Scissors";
         return string;
     }
-    else if (playerSelection==="Rock" && computerSelection==="Rock") {
-        string="Tie! Rock: Play again";
+    else if (playerSelection === "Rock" && computerSelection === "Rock") {
+        string = "Tie! Rock: Play again";
         return string;
     }
 
     //Section if player chooses paper
-    else if (playerSelection==="Paper" && computerSelection==="Paper") {
-        string="Tie! Paper: Play again";
+    else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        string = "Tie! Paper: Play again";
         return string;
     }
-    else if (playerSelection==="Paper" && computerSelection==="Scissors") {
-        string="You Lose! Scissors beats Paper";
+    else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        string = "You Lose! Scissors beats Paper";
         return string;
     }
-    else if (playerSelection==="Paper" && computerSelection==="Rock") {
-        string="You Win! Paper beats Rock";
+    else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        string = "You Win! Paper beats Rock";
         return string;
     }
 
     //Section if player chooses scissors
-    else if (playerSelection==="Scissors" && computerSelection==="Paper") {
-        string="You Win! Scissors beats Paper";
+    else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        string = "You Win! Scissors beats Paper";
         return string;
     }
-    else if (playerSelection==="Scissors" && computerSelection==="Scissors") {
-        string="Tie! Scissors: Play again";
+    else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        string = "Tie! Scissors: Play again";
         return string;
     }
     else {
-        string="You Lose! Rock beats Scissors";
+        string = "You Lose! Rock beats Scissors";
         return string;
     }
 }
 
 
 //This function will take the playRound function and run it five times
-function game() {
-    for (let i=0; i<5; i++) {
+//function game() {
+//    for (let i=0; i<5; i++) {
+//
+//        //Logging the selections from the player and computer
+//        playerSelection=upper(prompt("Pick a move: "));
+//        console.log("Player: "+playerSelection);
+//        computerSelection=computerPlay();
+//        console.log("Computer: "+computerSelection);
+//
+//        console.log(playRound(playerSelection, computerSelection));
+//    }
+//}
 
-        //Logging the selections from the player and computer
-        playerSelection=upper(prompt("Pick a move: "));
-        console.log("Player: "+playerSelection);
-        computerSelection=computerPlay();
-        console.log("Computer: "+computerSelection);
-
-        console.log(playRound(playerSelection, computerSelection));
-    }
-}
 //Calls the game() function to play 5 games
-game()
+//game()
+
+
+// The parameters that function playRound() takes
+let playerSelection = 'Rock';
+let computerSelection = computerPlay();
+
+
+//Event listeners for buttons
+
+const btn1 = document.querySelector('#rock');
+btn1.addEventListener('click', console.log(playRound(playerSelection, computerSelection)));
+console.log(btn1);
